74cf915aee49ffab5000bb7332e025407b387ca5
Jose Manuel Rodriguez Montes <jose92rm@gmail.com>
2014-11-27 11:14:46 +0100

    first commit

---
+++ cam-seg.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+import numpy as np
+import cv2
+import cv
+import time
+
+def comprobarFichero():
+  
+  existe = False
+  archivo = "//path/a/algun/fichero"
+  
+  try: 
+    fichero = open(archivo) 
+    fichero.close()
+    existe = True 
+  except: 
+    existe = False
+    
+  return existe
+
+tamanoMax = 400
+movimiento = False
+
+cam = cv2.VideoCapture(0)
+
+for x in xrange(1,100):
+  ret, frame = cam.read()
+  
+fgbg = cv2.BackgroundSubtractorMOG()
+
+while True:
+  ret, frame = cam.read()
+  
+  if ret == True:
+    frame = cv2.resize(frame, (tamanoMax,tamanoMax))
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bilateralFilter(gray, 11, 17, 17)
+    blur = cv2.blur(gray,(10,10))
+    
+    fgmask = fgbg.apply(blur) # Aplicamos la mascara
+    
+    columnas = cv2.reduce(fgmask,0,cv.CV_REDUCE_MAX)
+    filas = cv2.reduce(fgmask,1,cv.CV_REDUCE_MAX)
+    
+    for x in xrange(1, len(columnas[0])):
+      if columnas[0][x] > 0:
+	movimiento = True
+	break
+    if movimiento == False:
+      for x in xrange(1, len(filas)):
+	if filas[x,0] > 0:
+	  movimiento = True
+	  break
+    
+    if movimiento == True and comprobarFichero():
+      nombre = time.strftime("%y%m%d-%H:%M:%S")+".jpg"
+      path="/path/donde/guardar/imagenes/"+nombre
+      cv2.imwrite(path,frame)
+    
+    movimiento = False
+      
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
+    
+cv2.destroyAllWindows()
